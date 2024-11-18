a52dd9fc2092b49074997078d1b350447c61a72b
Agung Shandy Utama <agung.utama@moji.co.id>
2024-11-18 07:51:10 +0700

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 function Home() {
     return (
-        <div className="home container inline-flex">
-            <div className="bg-black size-48 m-5"></div>
-            <div className="bg-slate-400 size-48 m-5"></div>
-            <div className="bg-zinc-600 size-48 m-5 rounded-full"></div>
-            <div className="bg-slate-400 size-48 m-5"></div>
+        <div className="home container">
+            <div className="grid">
+                <div className="bg-black size-[50rem] m-5"></div>
+                <div className="bg-slate-400 size-48 m-5"></div>
+                <div className="bg-zinc-600 size-48 m-5 rounded-full"></div>
+                <div className="bg-slate-400 size-48 m-5"></div>
+            </div>
         </div>
     )
 }
